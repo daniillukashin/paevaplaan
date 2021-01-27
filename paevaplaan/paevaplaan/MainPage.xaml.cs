61dff1934aae510b2e1e51d8a03329da7e152c9c
Daniil <Daniil@DESKTOP-DGM97EK>
2021-01-27 22:03:18 +0200

    Добавьте файлы проекта.

---
+++ paevaplaan/paevaplaan/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace paevaplaan
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            StackLayout st = new StackLayout();
+            Title = "Моя неделя";
+            BackgroundColor = Color.Black;
+            Button pon_btn = new Button { Text = "Понедельник", BackgroundColor = Color.Yellow};
+            Button vtor_btn = new Button { Text = "Вторник", BackgroundColor = Color.Yellow};
+            Button sred_btn = new Button { Text = "Среда", BackgroundColor = Color.Yellow};
+            Button chet_btn = new Button { Text = "Четверг", BackgroundColor = Color.Yellow};
+            Button pjat_btn = new Button { Text = "Пятница", BackgroundColor = Color.Yellow};
+            Button subb_btn = new Button { Text = "Суббота", BackgroundColor = Color.Yellow};
+            Button vos_btn = new Button { Text = "Воскресенье", BackgroundColor = Color.Yellow};
+            pon_btn.Clicked += new EventHandler(Buttons_Clicked);
+            vtor_btn.Clicked += new EventHandler(Buttons_Clicked);
+            sred_btn.Clicked += new EventHandler(Buttons_Clicked);
+            chet_btn.Clicked += new EventHandler(Buttons_Clicked);
+            pjat_btn.Clicked += new EventHandler(Buttons_Clicked);
+            subb_btn.Clicked += new EventHandler(Buttons_Clicked);
+            vos_btn.Clicked += new EventHandler(Buttons_Clicked);
+            st.Children.Add(pon_btn);
+            st.Children.Add(vtor_btn);
+            st.Children.Add(sred_btn);
+            st.Children.Add(chet_btn);
+            st.Children.Add(pjat_btn);
+            st.Children.Add(subb_btn);
+            st.Children.Add(vos_btn);
+
+            Content = st;
+        }
+        private async void Buttons_Clicked(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            switch (btn.Text)
+            {
+                case "Понедельник":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new ponedejlnik());
+                    break;
+                case "Вторник":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new vtornik());
+                    break;
+                case "Среда":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new sreda());
+                    break;
+                case "Четверг":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new tsetverg());
+                    break;
+                case "Пятница":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new pjatnitsa());
+                    break;
+                case "Суббота":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new subbota());
+                    break;
+                case "Воскресенье":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new voskresenje());
+                    break;
+            }
+        }
+    }
+}
---
+++ paevaplaan/paevaplaan/vtornik.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace paevaplaan
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class vtornik : ContentPage
+    {
+        public vtornik()
+        {
+            Title = "Вторник";
+            BackgroundColor = Color.Black;
+            string[] tasks = new string[] { "Подъем", "Душ", "Завтрак", "Работа", "Обед", "Прогулка", "Ужин", "Отдых", "Сон" };
+            ListView list = new ListView();
+            Button back = new Button { Text = "Назад", BackgroundColor = Color.Yellow};
+            back.Clicked += Back_Clicked;
+            Button next = new Button { Text = "Далее", BackgroundColor = Color.Yellow};
+            next.Clicked += Next_Clicked; ;
+            list.ItemsSource = tasks;
+            list.ItemSelected += List_ItemSelected;
+            Content = new StackLayout { Children = { list, back, next } };
+        }
+
+        private async void Next_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new sreda());
+        }
+
+        private async void Back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        string kell;
+        private async void List_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                string text = e.SelectedItem.ToString();
+                if (e.SelectedItemIndex == 0)
+                {
+                    kell = "11:00";
+                }
+                else if (e.SelectedItemIndex == 1)
+                {
+                    kell = "11:30";
+                }
+                else if (e.SelectedItemIndex == 2)
+                {
+                    kell = "12:00";
+                }
+                else if (e.SelectedItemIndex == 3)
+                {
+                    kell = "13:00";
+                }
+                else if (e.SelectedItemIndex == 4)
+                {
+                    kell = "17:15";
+                }
+                else if (e.SelectedItemIndex == 5)
+                {
+                    kell = "17:45";
+                }
+                else if (e.SelectedItemIndex == 6)
+                {
+                    kell = "20:00";
+                }
+                else if (e.SelectedItemIndex == 7)
+                {
+                    kell = "20:30";
+                }
+                else if (e.SelectedItemIndex == 8)
+                {
+                    kell = "21:00";
+                }
+                else if (e.SelectedItemIndex == 9)
+                {
+                    kell = "1:00";
+                }
+                await DisplayAlert(kell, text, "Да");
+            }
+        }
+    }
+}
